f6a486c6de6d2a3bc4a2beff4d9a5b07201a3d16
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-01 17:55:10 +0600

    Basic Data Types

---
+++ module/src/dataType.ts
@@ -0,0 +1,25 @@
+
+// primitive type 
+ //string
+ let Name : string = "manik"
+ //number 
+ let role : number = 11;
+//boolean 
+let isAdmin : boolean = true;
+//undefined
+let x :undefined= undefined;
+//null
+let y:null=null;
+
+// no-primitive type
+
+// Array
+
+let friendList : string[] = ["manik",'nahid'];
+friendList.push("mahi");
+
+let friendRole : number[] = [2, 5, 5,];
+friendRole.push(1)
+
+//tuple 
+let user : [string, number, boolean]=['manik', 3 , true]
