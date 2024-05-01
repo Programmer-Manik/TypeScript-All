531fd07532997e0145f8aacae085d358c022f561
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-01 17:58:49 +0600

    git pull origin main

---
+++ module/dist/dataType.js
@@ -0,0 +1,20 @@
+"use strict";
+// primitive type 
+//string
+let Name = "manik";
+//number 
+let role = 11;
+//boolean 
+let isAdmin = true;
+//undefined
+let x = undefined;
+//null
+let y = null;
+// no-primitive type
+// Array
+let friendList = ["manik", 'nahid'];
+friendList.push("mahi");
+let friendRole = [2, 5, 5,];
+friendRole.push(1);
+//tuple 
+let user = ['manik', 3, true];
