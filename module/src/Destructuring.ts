42959e4bed89b81ae333cee10d9c75c4949dea20
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-01 19:25:32 +0600

    Destructuring In Typescript

---
+++ module/src/Destructuring.ts
@@ -0,0 +1,21 @@
+{
+  // Destructuring In Typescript
+  const user = {
+    id: 123,
+    name: {
+      first: "John",
+      last: "Doe",
+    },
+    address: "123 Main St, City, Country",
+    contact: 100000,
+  };
+
+  const {
+    contact,
+    name: { last: LastName },
+  } = user;
+
+
+  const friendList = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank"];
+  const [,,charlieData, ...rest] = friendList;
+}
