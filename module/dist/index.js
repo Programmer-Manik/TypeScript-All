08637605645bbce31f422efca54d60d23ad5ffd5
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-01 17:28:44 +0600

    Write Your First Typescript Program

--- module/dist/index.js
+++ module/dist/index.js
@@ -1,3 +1,8 @@
 "use strict";
 let course = "next level apollo developer";
 console.log(course);
+const apollo = (data) => {
+    const dataAll = data;
+    console.log(dataAll);
+};
+apollo("111");
--- module/src/index.ts
+++ module/src/index.ts
@@ -1,2 +1,9 @@
 let course:string = "next level apollo developer"
 console.log(course);
+
+const apollo = (data:string) => {
+   const dataAll = data ;
+  console.log(dataAll)
+}
+
+apollo("111")
