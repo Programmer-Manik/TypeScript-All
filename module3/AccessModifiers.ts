5e378ac555e4d2e190c00d650093e334844439f6
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-05 13:27:50 +0600

    Access Modifiers and public private data modifiers

---
+++ module3/AccessModifiers.ts
@@ -0,0 +1,33 @@
+{
+   // Access Modifiers
+   class BankAccount {
+     public readonly id:string;
+      public name:string;
+      private _balance:string;
+      constructor(id:string,name:string,_balance:string){
+         this.id = id 
+         this.name = name 
+         this._balance = balance
+      }
+      addDeposit(amount:number){
+         this._balance = this._balance + amount
+      }
+      getBalance() {
+         return this._balance
+      }
+   }
+
+   const goribManusherAccount = new BankAccount('111' , 'manik', '200')
+   // goribManusherAccount.balance = 0 
+   goribManusherAccount.addDeposit(2)
+   const balance = goribManusherAccount.getBalance()
+   // console.log(balance)
+
+
+
+   class Student extends BankAccount {
+      test(){
+         this
+      }
+   }
+}
