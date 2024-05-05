5516bb73cf33527c203fd9e33b9290bf418db3e4
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-05-05 10:16:02 +0600

    Inheritance In OOP.

---
+++ module3/InheritanceInOOP.ts
@@ -0,0 +1,35 @@
+{
+   // Inheritance In OOP
+   class Parent {
+      constructor(public name:string, public age:string, public address:string){}
+      getSleep(numberOfHours:number){
+         console.log(`${this.name} will sleep for ${numberOfHours}`)
+      }
+   }
+   
+
+
+   class Student extends Parent {
+      constructor( name:string,  age:string,  address:string){
+         super(name,age,address,)
+      }
+     
+   }
+   const Student1 = new Student( 'manik', '22', 'rode')
+
+
+
+   class Teacher extends Parent {
+      designation :string;
+      constructor( name:string,  age:string, address:string, designation:string){
+         super(name, age , address, )
+         this.designation= designation
+      }
+      takeClass(numberOfHours:number){
+         console.log(`${this.name}will take ${numberOfHours}`)
+      }
+   }
+   const Teacher1 = new Teacher( 'manik', '40', 'Dhaka' ,"Pro" )
+  
+
+}
